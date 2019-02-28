cce51e0a037a0f41912bea0d45111b030e3933ce
stevechow <290689305@qq.com>
2019-02-28 09:26:52 +0800

    上传个人网站

---
+++ cs/js/background.cycle-custom.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 2500,
+
+
+
+	images: ['images/1.jpg',
+			 'images/2.jpg']
+
+
+
+
+
+
+
+});
